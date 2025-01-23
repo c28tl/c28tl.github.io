72d0240e4d491c5ae32001b2770a2d35f60ed39a
Tenzin Lhading <c28tl@c28tl-fvfhthccq6l4.dalton.org>
2025-01-23 10:53:05 -0500

    new journal UI

---
+++ api.js
@@ -0,0 +1,4 @@
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=0cfd0adbe8cc4c199f3370223f1feba7&client_secret=02bf86d8b809492182d0c6ed8e3567ba"
+
---
+++ pokeapicall.js
@@ -0,0 +1,52 @@
+// fetch("https://pokeapi.co/api/v2/pokemon/ogerpon")
+// 	.then(response => {
+// 		if(!response.ok) {
+// 			throw new Error("Could not fetch resource");
+// 		}
+// 		return response.json();
+// 	})
+// 	.then(data => console.log(data.id))
+// 	.catch(error => console.error(error));
+
+
+
+async function fetchData(){
+
+	console.log("working")
+
+	try{
+
+		const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+		if(!response.ok){
+			throw new Error("Could not find pokemon")
+		}
+		const data = await response.json();
+		const pokemonSprite = data.sprites.front_default;
+		const pokemonid = data.id;
+		const pokename = data.name.substring(0, 1).toUpperCase() + data.name.substring(1);
+		const imgElement = document.getElementById("pokemonSprite")
+		imgElement.src = pokemonSprite;
+		imgElement.style.display = "block";
+		names.innerText = pokename
+		ids.innerText = "# " + pokemonid
+		console.log(pokename);
+	}
+
+	catch(error){
+		console.error(error);
+	}
+}
+
+function keycheck(e){
+	let key = 13;
+    if (e.keyCode == key) {
+        fetchData();
+    }
+}
+
+function running(){
+	fetchData();
+}
